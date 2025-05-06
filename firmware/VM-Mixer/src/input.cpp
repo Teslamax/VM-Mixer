c115a9de4937cd4251f7df8cf57eea8b592ad9ed
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-05-06 16:49:54 -0500

    Update input.cpp

--- firmware/VM-Mixer/src/input.cpp
+++ firmware/VM-Mixer/src/input.cpp
@@ -1,6 +1,6 @@
 #include <Arduino.h>
 #include <MIDI.h>
-extern midi::MidiInterface<midi::DefaultSerialMIDI> MIDI;  // actually declared via MIDI_CREATE_DEFAULT_INSTANCE()
+//extern midi::MidiInterface<midi::DefaultSerialMIDI> MIDI;  // actually declared via MIDI_CREATE_DEFAULT_INSTANCE()
 #include "config.h"
 #include "input.h"
 #include "midi.h"
