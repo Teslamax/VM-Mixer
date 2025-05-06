78a16a71fca49dc5c0a45ca0f6bf0c95ec9ea8a8
Ryan Rasmussen <Teslamax@users.noreply.github.com>
2025-05-06 03:30:18 -0500

    Update midi.cpp

--- firmware/VM-Mixer/src/midi.cpp
+++ firmware/VM-Mixer/src/midi.cpp
@@ -1,25 +1,37 @@
-// midi_manager.cpp - Handles sending MIDI messages for Proto-HID
+#include <MIDI.h>
+MIDI_CREATE_INSTANCE(usbMIDI, MIDI);
+
 #include <Arduino.h>
-#include "MIDIUSB.h"
-#include "midi_manager.h"
+#include "midi.h"
 #include "cli.h"
 
 void send_midi_cc(uint8_t cc_number, uint8_t value) {
-  midiEventPacket_t event = {0x0B, 0xB0, cc_number, value};
-  MidiUSB.sendMIDI(event);
-  MidiUSB.flush();
+  MIDI.sendControlChange(cc_number, value, 1);  // channel 1
   logInfo("MIDI CC Sent: CC%d -> %d", cc_number, value);
 }
 
 void send_midi_note(uint8_t note_number, bool on) {
   if (on) {
-    midiEventPacket_t noteOn = {0x09, 0x90, note_number, 127};
-    MidiUSB.sendMIDI(noteOn);
+    MIDI.sendNoteOn(note_number, 127, 1);  // velocity 127 on channel 1
     logInfo("MIDI Note On Sent: Note %d", note_number);
   } else {
-    midiEventPacket_t noteOff = {0x08, 0x80, note_number, 0};
-    MidiUSB.sendMIDI(noteOff);
+    MIDI.sendNoteOff(note_number, 0, 1);   // velocity 0 on channel 1
     logInfo("MIDI Note Off Sent: Note %d", note_number);
   }
-  MidiUSB.flush();
+}
+
+void poll_midi_feedback() {
+  if (MIDI.read()) {
+    // Handle incoming MIDI if needed
+    auto type = MIDI.getType();
+    if (type == midi::ControlChange) {
+      uint8_t cc = MIDI.getData1();
+      uint8_t value = MIDI.getData2();
+      logInfo("MIDI IN: CC%d -> %d", cc, value);
+    } else if (type == midi::NoteOn || type == midi::NoteOff) {
+      uint8_t note = MIDI.getData1();
+      uint8_t velocity = MIDI.getData2();
+      logInfo("MIDI IN: %s Note %d Vel %d", (type == midi::NoteOn ? "On" : "Off"), note, velocity);
+    }
+  }
 }
